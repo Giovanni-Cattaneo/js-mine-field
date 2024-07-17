9ec1402c3666d50852e84bff0c4390c703ae8cc9
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-07-17 11:30:30 +0200

    edit box generation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@ let container = document.getElementById("container");
 
 const markup = `<div class="box"></div>`
 
+const skull = `<i class="fa-solid fa-skull></i>"`
+
+const flower = `<i class="fa-solid fa-flower></i>`
+
 let boxes = document.getElementsByClassName("box")
 
 
@@ -14,8 +18,26 @@ for (let index = 0; index < 100; index++) {
 
 boxes = Array.from(boxes)
 
-// console.log(boxes)
 
-boxes.forEach(box => {
-    box.innerText = Math.floor(Math.random() * 100)
-});
+
+function randomUniqueNum(range, outputCount) {
+
+    let arr = []
+    for (let i = 1; i <= range; i++) {
+        arr.push(i)
+    }
+
+    let result = [];
+
+    for (let i = 1; i <= outputCount; i++) {
+        const random = Math.floor(Math.random() * (range - i));
+        result.push(arr[random]);
+        arr[random] = arr[range - i];
+    }
+
+    console.log(result)
+
+    return result;
+}
+
+randomUniqueNum(100, 100)
